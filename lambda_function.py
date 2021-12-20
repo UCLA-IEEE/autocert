b57be11bf56dc04fa508ebf4cce9b634404127a0
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-12-20 10:01:13 -0800

    Add cert generation and installation logic

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,61 @@
+import certbot
+import logging
+import shutil
+
+from cpanel import cpanel
+from constants import *
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_cert(domain):
+    '''Given a domain, run Certbot to obtain a certificate.
+    '''
+    logger.info('Obtaining cert for %s', domain)
+    args = [
+        'certonly',
+        '--manual',
+        '--domains', domain,
+        '--preferred-challenges', 'http',
+        '--non-interactive',
+        '--manual-public-ip-logging-ok',
+        '--agree-tos',
+        '--force-renewal',
+        '--email', CERT_EMAIL,
+        # Override directories to allow running without root
+        '--config-dir', '/tmp/certbot/config',
+        '--work-dir', '/tmp/certbot/work',
+        '--logs-dir', '/tmp/certbot/logs',
+        # Set verification hooks
+        '--manual-auth-hook', 'python3 auth-hook.py',
+        '--manual-cleanup-hook', 'python3 cleanup-hook.py',
+    ]
+    code = int(certbot.main.main(args))
+    logger.info('Certbot exit code: %d', code)
+    if code != 0:
+        raise RuntimeError(f'Certbot failed to generate a cert for {domain}')
+
+def install_cert(domain):
+    '''Fetch the server certificate and private key from
+    /tmp/certbot/config/live/<domain>, then use the CPanel UAPI to install it. On
+    success, delete the certificate information.
+    '''
+    parameters = {'domain': domain}
+    base_dir = f'/tmp/certbot/config/live/{domain}/'
+    with open(base_dir + 'cert.pem') as f:
+        parameters['cert'] = ''.join(f.readlines()).strip()
+    with open(base_dir + 'privkey.pem') as f:
+        parameters['key'] = ''.join(f.readlines()).strip()
+
+    logger.info('Installing cert for %s', domain)
+    try:
+        cpanel('SSL/install_ssl', parameters)
+    except RuntimeError as e:
+        raise RuntimeError(f'Failed to install cert for {domain}: {e}')
+
+    shutil.rmtree(base_dir)
+
 def lambda_handler(event, context):
-    pass
+    for domain in WEBROOTS:
+        get_cert(domain)
+        install_cert(domain)
